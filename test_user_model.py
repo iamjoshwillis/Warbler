3adf9df4dda0add135628174283398b734c2297f
Josh Willis <josh@infinitysoundfactory.com>
2024-01-16 09:41:19 -0500

    InitialAdd

---
+++ test_user_model.py
@@ -0,0 +1,93 @@
+"""User model tests."""
+
+# run these tests like:
+#
+#    python -m unittest test_user_model.py
+
+
+import os
+from unittest import TestCase
+from sqlalchemy import exc
+
+from models import db, User, Message, Follows
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+# Now we can import app
+
+from app import app
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+
+        self.client = app.test_client()
+
+    def test_user_model(self):
+        """Does basic model work?"""
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        # User should have no messages & no followers
+        self.assertEqual(len(u.messages), 0)
+        self.assertEqual(len(u.followers), 0)
+        
+    def test_correct_signup(self):
+        u_test = User.signup("testing", "testing@test.test", "testpassword", None)
+        uid = 10101
+        u_test.id = uid
+        db.session.commit()
+
+        u_test = User.query.get(uid)
+        self.assertIsNotNone(u_test)
+        self.assertEqual(u_test.username, "testtesttest")
+        self.assertEqual(u_test.email, "testtest@test.com")
+        self.assertNotEqual(u_test.password, "password")
+        self.assertTrue(u_test.password.startswith("$2b$"))
+
+    def test_invalid_username_signup(self):
+        invalid = User.signup(None, "test@test.com", "password", None)
+        uid = 123456789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_invalid_email_signup(self):
+        invalid = User.signup("testtest", None, "password", None)
+        uid = 123789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_invalid_password_signup(self):
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@email.com", "", None)
+        
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@email.com", None, None)
---
+++ test_user_views.py
@@ -0,0 +1,106 @@
+"""User View tests."""
+
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Message, User, Likes, Follows
+from bs4 import BeautifulSoup
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+# Now we can import app
+
+from app import app, CURR_USER_KEY
+
+db.create_all()
+
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+
+        self.testuser = User.signup(username="testuser",
+                                    email="test@test.com",
+                                    password="testuser",
+                                    image_url=None)
+        self.testuser_id = 8989
+        self.testuser.id = self.testuser_id
+
+        self.u1 = User.signup("abc", "test1@test.com", "password", None)
+        self.u1_id = 778
+        self.u1.id = self.u1_id
+        self.u2 = User.signup("efg", "test2@test.com", "password", None)
+        self.u2_id = 884
+        self.u2.id = self.u2_id
+        self.u3 = User.signup("hij", "test3@test.com", "password", None)
+        self.u4 = User.signup("testing", "test4@test.com", "password", None)
+
+        db.session.commit()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+
+    def test_users_index(self):
+        with self.client as c:
+            resp = c.get("/users")
+
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertIn("@efg", str(resp.data))
+            self.assertIn("@hij", str(resp.data))
+            self.assertIn("@testing", str(resp.data))
+
+    def test_users_search(self):
+        with self.client as c:
+            resp = c.get("/users?q=test")
+
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@testing", str(resp.data))            
+
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertNotIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
+
+    def test_user_show(self):
+        with self.client as c:
+            resp = c.get(f"/users/{self.testuser_id}")
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("@testuser", str(resp.data))
+
+    def test_unauthorized_following_page_access(self):
+        self.setup_followers()
+        with self.client as c:
+
+            resp = c.get(f"/users/{self.testuser_id}/following", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertIn("Access unauthorized", str(resp.data))
+
+    def test_unauthorized_followers_page_access(self):
+        self.setup_followers()
+        with self.client as c:
+
+            resp = c.get(f"/users/{self.testuser_id}/followers", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertIn("Access unauthorized", str(resp.data))
